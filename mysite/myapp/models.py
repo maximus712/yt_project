053e6c06048a36ac56f7233ec1e36785fe38952f
maximus712 <85500338+maximus712@users.noreply.github.com>
2023-10-14 16:17:49 +0300

    add image to app

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 # Create your models here.
@@ -6,7 +7,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.CharField(max_length=200)
+    image = models.ImageField(blank=True, upload_to = "images")
     
     def __str__(self):
         return self.name
+
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from myapp import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include('myapp.urls',namespace='myapp'))
    
 ]
+
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
